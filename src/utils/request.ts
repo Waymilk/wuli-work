72ecd81dbf99066bab6948b5da9fd0ba499049a1
milky.peng <745309089@qq.com>
2026-06-03 09:47:08 +0800

    feat:添加全局 401 处理

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { pinia } from '@/stores/pinia'
+import { useAuthStore } from '@/stores/auth'
 
 const configuredBaseURL = import.meta.env.VITE_API_BASE_URL || '/'
 const useDevProxy = import.meta.env.DEV && import.meta.env.VITE_USE_DEV_PROXY !== '0'
@@ -19,7 +21,14 @@ request.interceptors.request.use((config) => {
 
 request.interceptors.response.use(
   (response) => response.data,
-  (error) => Promise.reject(error),
+  (error) => {
+    if (error?.response?.status === 401) {
+      const authStore = useAuthStore(pinia)
+      authStore.clearAuth()
+      authStore.openAuthModal()
+    }
+    return Promise.reject(error)
+  },
 )
 
 export default request
